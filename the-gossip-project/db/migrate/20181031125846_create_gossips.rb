7f2fc313abb8256238f843912bc7c8716ed9da37
Alexandre de Temmerman <alexandre2t@hotmail.fr>
2018-11-02 00:20:32 +0100

    ajustement de la fonction suppression

--- the-gossip-project/db/migrate/20181031125846_create_gossips.rb
+++ the-gossip-project/db/migrate/20181031125846_create_gossips.rb
@@ -1,7 +1,7 @@
 class CreateGossips < ActiveRecord::Migration[5.2]
   def change
     create_table :gossips do |t|
-      t.belongs_to :user, optional: true
+      t.belongs_to :user
       t.string :anonymous
       t.string :title
       t.text :content
